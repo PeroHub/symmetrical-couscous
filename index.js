f9167ed47bda1cd50a99f960500efbe8e04d620a
PeroHub <peterime600@gmail.com>
2023-07-20 19:41:46 +0100

    nacos day

---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * navbar toggle
+ */
+
+const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+
+const navElemArr = [overlay, navOpenBtn, navCloseBtn];
+
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
+
+
+/**
+ * add active class on header when scrolled 200px from top
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 200 ? header.classList.add("active")
+    : header.classList.remove("active");
+})
+
+const shop = document.querySelector(".shop")
+const pro = document.querySelector(".pro")
+
+shop.addEventListener("click", () => {
+  pro.scrollIntoView(true)
+})
+
+const contactBody = document.querySelector(".contactBody")
+const contactNav = document.querySelector(".contactNav")
+
+contactNav.addEventListener("click", () => {
+  contactBody.scrollIntoView(true)
+})
+
